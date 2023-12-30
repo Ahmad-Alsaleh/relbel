44065097e682b1e7fe74eaf38badfecca9794807
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2023-12-31 01:44:43 +0400

    test: add test for get_single_sigma_sq_and_mu()

---
+++ tests/testthat/test-get_single_sigma_sq_and_mu.R
@@ -0,0 +1,16 @@
+test_that("get_single_sigma_sq_and_mu() works", {
+  mu_0 <- 7
+  lambda_0 <- 0.875
+  alpha_0 <- 2.105783
+  beta_0 <- 15.11126
+
+  actual_result <- withr::with_seed(42, {
+    get_single_sigma_sq_and_mu(mu_0, lambda_0, alpha_0, beta_0)
+  })
+
+  expected_result <- list(
+    sigma_sq = 3.963156,
+    mu = 6.01634
+  )
+  expect_equal(actual_result, expected_result, tolerance = 1e-5)
+})

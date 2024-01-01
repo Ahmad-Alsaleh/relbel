976bd82034fd73ac7c478c57e24487cb627b21d0
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2024-01-01 20:20:03 +0400

    test: add test for `get_single_sigma_sq_mu_post()`

--- tests/testthat/test-get_single_sigma_sq_and_mu.R
+++ tests/testthat/test-get_single_sigma_sq_and_mu.R
@@ -14,3 +14,24 @@ test_that("get_single_sigma_sq_mu_prior() works", {
   )
   expect_equal(actual_result, expected_result, tolerance = 1e-5)
 })
+
+test_that("get_single_sigma_sq_mu_post() works", {
+  feature <- c(
+    0.97, 0.72, 1.00, 0.81, 0.62, 1.32, 1.24, 0.99, 0.90, 0.74, 0.88, 0.94,
+    1.16, 0.86, 0.85, 0.58, 0.57, 0.64, 0.98, 1.09, 0.92, 0.78, 1.24, 1.18
+  )
+  mu_0 <- 1
+  lambda_0 <- 0.25
+  alpha_0 <- 6.454639
+  beta_0 <- 26.84146
+
+  actual_result <- withr::with_seed(42, {
+    get_single_sigma_sq_mu_post(mu_0, lambda_0, alpha_0, beta_0, feature)
+  })
+
+  expected_result <- list(
+    sigma_sq = 1.13083,
+    mu = 0.8545522
+  )
+  expect_equal(actual_result, expected_result, tolerance = 1e-5)
+})
